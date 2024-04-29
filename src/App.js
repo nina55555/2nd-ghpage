b3a7951a2ff86dd4fa2f4eaa7c8a32149e116a1a
Nina <morville5audrey@gmail.com>
2024-04-29 17:13:21 +0200

    photo love sentence test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           LOVE <code>accomplishment</code> and faith to live.
         </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolores inventore placeat corporis ratione sapiente et esse ab ad. Distinctio provident vitae nemo eius tempora!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
